6c17e06da6323c7289bea13d244cb64fd35bb803
lonskyne <lonskyne@gmail.com>
2024-03-01 14:59:46 +0100

    Added ability to choose folder.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
+use rfd::FileDialog;
+
 slint::slint! {
     import {Button, VerticalBox } from "std-widgets.slint";
 
     export component App inherits Window{
-        property <int> counter: 1;
+        in property <string> current_folder;
 
-        Text { text :"Hello world!"; }
-        
+        callback choose_folder_clicked <=> choose_folder_btn.clicked;
+
+        Text { text : "Current folder: " + current_folder; }
+
+        choose_folder_btn := Button { text: "Choose folder"; }
     }
 }
 
 fn main() {
-    App::new().unwrap().run().unwrap();
-    println!("Hello, world!");
+    let mut folderPath : std::path::PathBuf = std::path::PathBuf::new();
+
+    let app : App = App::new().unwrap();
+    let weak = app.as_weak();
+
+    app.on_choose_folder_clicked( move || {
+        let app : App = weak.upgrade().unwrap();
+        
+        let folder = FileDialog::new()
+            .set_directory("/")
+            .pick_folder();
+
+        match folder { 
+            Some(f) => folderPath = f,
+            None => (),
+        }
+    }
+    );
+
+    app.run().unwrap();
 }
