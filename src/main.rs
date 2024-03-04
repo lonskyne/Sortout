596dd800460c611e1fc1c31c760ac54cf7c86086
lonskyne <viktor@davidovic.io>
2024-03-04 21:41:45 +0100

    Can now open directory wich prints all files and folders in the current directory.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,64 @@
 use rfd::FileDialog;
+use std::rc:Rc;
+use std::cell::RefCell;
+use std::fs;
+use std::path::Path;
+
 
 slint::slint! {
     import {Button, VerticalBox } from "std-widgets.slint";
 
     export component App inherits Window{
         in property <string> current_folder;
+        in property <string> current_file;
 
-        callback choose_folder_clicked <=> choose_folder_btn.clicked;
+        callback choose_folder <=> choose_folder_btn.clicked;
+        callback open_folder <=> open_folder_btn.clicked;
 
-        Text { text : "Current folder: " + current_folder; }
+        VerticalBox {
+            Text { text : "Current folder: " + current_folder; }
 
-        choose_folder_btn := Button { text: "Choose folder"; }
+            choose_folder_btn := Button { text: "Choose folder"; }
+
+            open_folder_btn := Button { text: "Open folder"; }
+
+            Text { text: "Current file: " + current_file; }
+        }
     }
 }
 
 fn main() {
-    let mut folderPath : std::path::PathBuf = std::path::PathBuf::new();
+    let folder_path = Rc::new(RefCell::new(String::from("")));
 
     let app : App = App::new().unwrap();
     let weak = app.as_weak();
 
-    app.on_choose_folder_clicked( move || {
-        let app : App = weak.upgrade().unwrap();
-        
-        let folder = FileDialog::new()
-            .set_directory("/")
-            .pick_folder();
+    let mut fp_copy = folder_path.clone();
 
-        match folder { 
-            Some(f) => folderPath = f,
-            None => (),
+    app.on_choose_folder( {
+        let app : App = weak.upgrade().unwrap();
+
+        move || {
+            let folder = FileDialog::new()
+                .set_directory("/")
+                .pick_folder();
+
+            match folder { 
+                Some(f) => { (*fp_copy.borrow_mut()).replace(f.clone().into_os_string().into_string().unwrap()); 
+                                        app.set_current_folder(f.clone().into_os_string().into_string().unwrap().into()); }, 
+                None => (),
+            };
+        }
+    }
+    );
+
+    let fp_copy = folder_path.clone();
+    app.on_open_folder( move || {
+        let app : App = weak.upgrade().unwrap();     
+        let paths = fs::read_dir(Path::new::<str>(&(*<RefCell<String> as Clone>::clone(&fp_copy).into_inner()))).unwrap();
+
+        for path in paths {
+            println!("Name: {}", path.unwrap().path().display())
         }
     }
     );
