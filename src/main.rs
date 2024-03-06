b8ed8fbe8ef673833a225c29bc33382d70c7f301
lonskyne <viktor@davidovic.io>
2024-03-06 12:17:46 +0100

    Add ability to go back and fourth trough files in a folder.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rfd::FileDialog;
-use std::{borrow::{Borrow, BorrowMut}, fs::DirEntry, rc::Rc};
+use std::{borrow::{BorrowMut}, fs::DirEntry, rc::Rc};
 use std::cell::RefCell;
 use std::fs;
 use std::path::Path;
@@ -36,10 +36,6 @@ slint::slint! {
         }
     }
 }
-/* 
-fn update_file(fi: i32, cf_mut: &mut Rc<RefCell<String>>, ) {
-    cf_mut.replace(t)
-}t*/
 
 fn main() {
     let folder_path = Rc::new(RefCell::new(String::from("")));
@@ -48,8 +44,10 @@ fn main() {
     let app : App = App::new().unwrap();
     let weak = app.as_weak();
 
-    let mut file_index = Rc::new(RefCell::new(0));let paths_vec: Vec<Result<DirEntry, Error>>
-    let mut max_index = 0;
+    let file_index = Rc::new(RefCell::new(0));
+    let mut paths_vec: Rc<RefCell<Vec<Result<DirEntry, Error>>>> = Rc::new(RefCell::new(Vec::new()));
+    let max_index = 0;
+
 
     let mut fp_copy = folder_path.clone();
 
@@ -73,37 +71,68 @@ fn main() {
 
     let fp_copy = folder_path.clone();
     let mut cf_copy = current_file.clone();
+    let mut pv_copy = paths_vec.clone();
 
-    app.on_open_folder( move || {
+    app.on_open_folder( {
         let app : App = weak.upgrade().unwrap();
-        
-        let paths = fs::read_dir(Path::new::<str>(&(*<RefCell<String> as Clone>::clone(&fp_copy).into_inner()))).unwrap();
 
-        let paths_vec: Vec<Result<DirEntry, Error>> = paths.collect();
+        move ||  {
+            let paths_wrapped = fs::read_dir(Path::new::<str>(&(*<RefCell<String> as Clone>::clone(&fp_copy).into_inner())));
 
-        (*cf_copy.borrow_mut()).replace(paths_vec[0].as_ref().unwrap().file_name().into_string().unwrap().into());
-        app.set_current_file(paths_vec[0].as_ref().unwrap().file_name().into_string().unwrap().into());
+            match paths_wrapped {
+                Ok(f) => { pv_copy.replace(f.collect()); (); },
+                Err(e) => println!("{:?}", e),
+            };
 
-        //sortirati nekad paths po datumu kreacije
-    }
-    );
+            let mut val : String = String::new();
+
+            if pv_copy.borrow().len() > 0 {
+                val = pv_copy.borrow()[0].as_ref().unwrap().file_name().into_string().unwrap();
+            }
+
+            (*cf_copy.borrow_mut()).replace(val.clone());
+            app.set_current_file(val.clone().into());
+
+            //sortirati nekad paths po datumu kreacije
+        }
+    });
+
 
     app.on_next_file({
+        let app : App = weak.upgrade().unwrap();
+
+        let mut cf_copy = current_file.clone();
         let mut fi_copy: Rc<RefCell<i32>> = file_index.clone();
+        let pv_copy = paths_vec.clone();
 
         move || {
             fi_copy.borrow_mut().replace_with(|&mut x| x + 1);
+
+            let new_val = *fi_copy.borrow();
+
+            (*cf_copy.borrow_mut()).replace(pv_copy.clone().borrow()[new_val as usize].as_ref().unwrap().file_name().into_string().unwrap().into());
+            app.set_current_file(pv_copy.clone().borrow()[new_val as usize].as_ref().unwrap().file_name().into_string().unwrap().into());
         }
     });
 
 
     app.on_prev_file({
-        let mut fi_copy = Rc::clone(&file_index);
+        let app : App = weak.upgrade().unwrap();
+
+        let mut cf_copy = current_file.clone();
+        let mut fi_copy: Rc<RefCell<i32>> = file_index.clone();
+        let pv_copy = paths_vec.clone();
 
         move || {
             fi_copy.borrow_mut().replace_with(|&mut x| x - 1);
+
+            let new_val = *fi_copy.borrow();
+
+            (*cf_copy.borrow_mut()).replace(pv_copy.borrow()[new_val as usize].as_ref().unwrap().file_name().into_string().unwrap().into());
+            app.set_current_file(pv_copy.borrow()[new_val as usize].as_ref().unwrap().file_name().into_string().unwrap().into());
         }
     });
 
+    
     app.run().unwrap();
 }
