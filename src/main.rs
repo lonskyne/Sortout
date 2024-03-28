565985436747673d61f74ab585cbd7306d72275d
lonskyne <lonskyne@gmail.com>
2024-03-28 10:47:05 +0100

    Separate slint code to a .slint file

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,13 @@
 use rfd::FileDialog;
-use slint::{ComponentHandle, Image};
 use std::{borrow::BorrowMut, fs::DirEntry, rc::Rc};
 use std::cell::RefCell;
 use std::fs::{self, read_to_string};
 use std::path::Path;
 use trash;
+use std::io::Error;
 
-use std::io::Error;slint::slint! {
-    import {Button, VerticalBox, HorizontalBox, ScrollView } from "std-widgets.slint";
-
-    export component App inherits Window {
-        in property <string> current_folder;
-        in property <string> current_file;
-        in property <string> current_file_type;
-
-        in property <string> current_file_content_text;
-        in property <image> current_file_content_image;
-
-        in property <string> marked_deletion_list;
-
-        // 
-        in property <bool> folder_chosen;
-        in property <bool> folder_opened;
-
-        callback choose_folder <=> choose_folder_btn.clicked;
-        callback open_folder <=> open_folder_btn.clicked;
-
-        callback prev_file <=> previous_file_btn.clicked;
-        callback next_file <=> next_file_btn.clicked;
-
-        callback mark_delete <=> mark_delete_btn.clicked;
-
-        callback confirm_marks <=> confirm_marks_btn.clicked;
-
-        VerticalBox {
-            Text { text : "Current folder: " + current_folder; }
-
-            choose_folder_btn := Button { text: "Choose folder"; }
-
-            open_folder_btn := Button { text: "Open folder"; enabled: folder_chosen; }
-
-            HorizontalBox {
-                VerticalBox {
-                    width: 10%;
-
-                    Text { text: "Marked for deletion:"; }
-
-                    Text { text: marked_deletion_list; }
-                }
-            }
-
-            confirm_marks_btn := Button { text: "Confirm marks"; enabled: folder_opened; }
-
-            VerticalBox {
-                Text { text : "Current file"; }
-                Text { text : "File name: " + current_file; }
-                Text { text: "File type: " + current_file_type; }
-                Text { text: "File contents: "; }
-                Text { text: current_file_content_text;}
-                Image { source: current_file_content_image; width: 60%; height: 60%; image-rendering: pixelated;}
-            }
-
-            HorizontalBox {
-                mark_delete_btn := Button { text: "Mark: Delete"; enabled: folder_opened; }
-            }
-
-            HorizontalBox {
-                previous_file_btn := Button { text: "<"; enabled: folder_opened; }
-
-                next_file_btn := Button { text: ">"; enabled: folder_opened; }
-            }
-        }
-
-
-        forward-focus: my-key-handler;
-
-        my-key-handler := FocusScope {
-            key-pressed(event) => {
-                if event.text == Key.LeftArrow { previous_file_btn.clicked(); }
-                if event.text == Key.RightArrow { next_file_btn.clicked(); }
-                if event.text == Key.Delete { mark_delete_btn.clicked(); }
-                accept
-            }
-        }
-    }
-}
+use slint::Image;
+slint::include_modules!();
 
 fn check_file_type(file_ext : &str) -> &'static str{
     let text_file_extensions = ["txt", "csv", "json", "c", "cpp", "py", "rs", "html", "css"];
@@ -92,7 +15,7 @@ fn check_file_type(file_ext : &str) -> &'static str{
 
     if text_file_extensions.contains(&file_ext.to_lowercase().as_str()) { return "text"; }
 
-    if image_file_extensions.contains(&file_ext.to_lowercase().as_str()) { return "image" };
+    if image_file_extensions.contains(&file_ext.to_lowercase().as_str()) { return "image"; }
     
     return "other";
 }
@@ -145,13 +68,13 @@ fn clear_app_ui(app_ref : &App) {
     app.set_current_file_content_image(Image::default());
 }
 
-fn main() {
+fn main() -> Result<(), slint::PlatformError> {
     let folder_path = Rc::new(RefCell::new(String::from("")));
     let current_file = Rc::new(RefCell::new(String::from("")));
 
     let marked_deletion = Rc::new(RefCell::new(Vec::<i32>::new()));
 
-    let app : App = App::new().unwrap();
+    let app = App::new()?;
     let weak = app.as_weak();
 
     let file_index = Rc::new(RefCell::new(0));
@@ -302,5 +225,5 @@ fn main() {
         }
     });
     
-    app.run().unwrap();
+    app.run()
 }
